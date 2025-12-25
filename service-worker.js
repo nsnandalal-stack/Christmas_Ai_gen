0288516ac24f76877ab923c45ef14ca5ded81948
nsnandalal-stack <nsnandalal@gmail.com>
2025-12-25 13:58:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,24 @@
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open("christmas").then(c=>c.addAll(["./","./index.html","./style.css","./script.js"])));
+const CACHE_NAME = "christmas-card-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./how-old-is-santa-claus.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
